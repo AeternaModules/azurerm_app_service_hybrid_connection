3ca8304151f6ac61f7a1c49b374c094eee9664b1
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:40 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,51 +1,46 @@
-output "app_service_hybrid_connections" {
-  description = "All app_service_hybrid_connection resources"
-  value       = azurerm_app_service_hybrid_connection.app_service_hybrid_connections
-  sensitive   = true
-}
 output "app_service_hybrid_connections_app_service_name" {
-  description = "List of app_service_name values across all app_service_hybrid_connections"
-  value       = [for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : v.app_service_name]
+  description = "Map of app_service_name values across all app_service_hybrid_connections, keyed the same as var.app_service_hybrid_connections"
+  value       = { for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : k => v.app_service_name }
 }
 output "app_service_hybrid_connections_hostname" {
-  description = "List of hostname values across all app_service_hybrid_connections"
-  value       = [for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : v.hostname]
+  description = "Map of hostname values across all app_service_hybrid_connections, keyed the same as var.app_service_hybrid_connections"
+  value       = { for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : k => v.hostname }
 }
 output "app_service_hybrid_connections_namespace_name" {
-  description = "List of namespace_name values across all app_service_hybrid_connections"
-  value       = [for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : v.namespace_name]
+  description = "Map of namespace_name values across all app_service_hybrid_connections, keyed the same as var.app_service_hybrid_connections"
+  value       = { for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : k => v.namespace_name }
 }
 output "app_service_hybrid_connections_port" {
-  description = "List of port values across all app_service_hybrid_connections"
-  value       = [for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : v.port]
+  description = "Map of port values across all app_service_hybrid_connections, keyed the same as var.app_service_hybrid_connections"
+  value       = { for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : k => v.port }
 }
 output "app_service_hybrid_connections_relay_id" {
-  description = "List of relay_id values across all app_service_hybrid_connections"
-  value       = [for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : v.relay_id]
+  description = "Map of relay_id values across all app_service_hybrid_connections, keyed the same as var.app_service_hybrid_connections"
+  value       = { for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : k => v.relay_id }
 }
 output "app_service_hybrid_connections_relay_name" {
-  description = "List of relay_name values across all app_service_hybrid_connections"
-  value       = [for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : v.relay_name]
+  description = "Map of relay_name values across all app_service_hybrid_connections, keyed the same as var.app_service_hybrid_connections"
+  value       = { for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : k => v.relay_name }
 }
 output "app_service_hybrid_connections_resource_group_name" {
-  description = "List of resource_group_name values across all app_service_hybrid_connections"
-  value       = [for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : v.resource_group_name]
+  description = "Map of resource_group_name values across all app_service_hybrid_connections, keyed the same as var.app_service_hybrid_connections"
+  value       = { for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : k => v.resource_group_name }
 }
 output "app_service_hybrid_connections_send_key_name" {
-  description = "List of send_key_name values across all app_service_hybrid_connections"
-  value       = [for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : v.send_key_name]
+  description = "Map of send_key_name values across all app_service_hybrid_connections, keyed the same as var.app_service_hybrid_connections"
+  value       = { for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : k => v.send_key_name }
 }
 output "app_service_hybrid_connections_send_key_value" {
-  description = "List of send_key_value values across all app_service_hybrid_connections"
-  value       = [for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : v.send_key_value]
+  description = "Map of send_key_value values across all app_service_hybrid_connections, keyed the same as var.app_service_hybrid_connections"
+  value       = { for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : k => v.send_key_value }
   sensitive   = true
 }
 output "app_service_hybrid_connections_service_bus_namespace" {
-  description = "List of service_bus_namespace values across all app_service_hybrid_connections"
-  value       = [for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : v.service_bus_namespace]
+  description = "Map of service_bus_namespace values across all app_service_hybrid_connections, keyed the same as var.app_service_hybrid_connections"
+  value       = { for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : k => v.service_bus_namespace }
 }
 output "app_service_hybrid_connections_service_bus_suffix" {
-  description = "List of service_bus_suffix values across all app_service_hybrid_connections"
-  value       = [for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : v.service_bus_suffix]
+  description = "Map of service_bus_suffix values across all app_service_hybrid_connections, keyed the same as var.app_service_hybrid_connections"
+  value       = { for k, v in azurerm_app_service_hybrid_connection.app_service_hybrid_connections : k => v.service_bus_suffix }
 }
 
